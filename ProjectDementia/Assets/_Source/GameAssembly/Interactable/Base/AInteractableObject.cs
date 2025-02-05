d9d2da75c14d6ba7922c150af14f0942e03b8c1b
Алексей <ix.fenix@mail.ru>
2025-02-05 18:01:38 +0300

    feat: interact text

--- ProjectDementia/Assets/_Source/GameAssembly/Interactable/Base/AInteractableObject.cs
+++ ProjectDementia/Assets/_Source/GameAssembly/Interactable/Base/AInteractableObject.cs
@@ -7,6 +7,7 @@ namespace Interactable.Base
     [RequireComponent(typeof(PhotonView))]
     public abstract class AInteractableObject : MonoBehaviour
     {
+        [field: SerializeField] public string InteractText { get; private set; }
         [field: SerializeField] public PhotonView PhotonView { get; private set; }
         
         public abstract event Action OnInteract;
--- ProjectDementia/Assets/_Source/GameAssembly/Player/PlayerRaycaster.cs
+++ ProjectDementia/Assets/_Source/GameAssembly/Player/PlayerRaycaster.cs
@@ -1,5 +1,6 @@
 ﻿using DG.Tweening;
 using Interactable.Base;
+using TMPro;
 using UnityEngine;
 using Utils;
 
@@ -8,15 +9,18 @@ namespace Player
     public class PlayerRaycaster : MonoBehaviour
     {
         [SerializeField] private float animCenterPointTime;
+        [SerializeField] private float animHelperTextTime;
         [SerializeField] private float centerPointMultiplier;
         [SerializeField] private RectTransform centerPoint;
-        [SerializeField] private GameObject interactHelper;
+        [SerializeField] private TMP_Text interactHelper;
         [SerializeField] private LayerMask interactableLayer;
         [SerializeField] private float rayDistance;
 
         private float _startSize;
         private Tween _growTween;
         private Tween _backTween;
+        private Tween _textAppearTween;
+        private Tween _textDisappearTween;
 
         private void Start()
         {
@@ -28,19 +32,28 @@ namespace Player
             if (!Physics.Raycast(transform.position, transform.forward, out var hit, rayDistance,
                     interactableLayer))
             {
-                interactHelper.gameObject.SetActive(false);
-                
                 if (_backTween != null && _backTween.IsActive())
                     return;
                 
                 _backTween = centerPoint.DOSizeDelta(new Vector2(_startSize, _startSize), animCenterPointTime);
                 if(!_growTween.IsActive())
                     _growTween.Kill();
+                
+                if (_textDisappearTween != null && _textDisappearTween.IsActive())
+                    return;
+                
+                _textDisappearTween = interactHelper.DOFade(0f, animHelperTextTime);
+                if(!_textAppearTween.IsActive())
+                    _textAppearTween.Kill();
 
                 return;
             }
 
-            interactHelper.gameObject.SetActive(true);
+            // If raycast object with given layer
+            
+            hit.transform.TryGetComponent<AInteractableObject>(out var interactable);
+            
+            interactHelper.text = interactable?.InteractText;
 
             var growScale = _startSize * centerPointMultiplier;
 
@@ -51,12 +64,19 @@ namespace Player
                     _backTween.Kill();
             }
 
+            if (_textAppearTween == null || !_textAppearTween.IsActive())
+            {
+                _textAppearTween = interactHelper.DOFade(1f, animHelperTextTime);
+                if(!_textDisappearTween.IsActive())
+                    _textDisappearTween.Kill();
+            }
+
             if (!Input.GetKeyDown(KeyCode.E))
                 return;
 
             if (hit.transform.gameObject &&
                 LayerService.CheckLayersEquality(hit.transform.gameObject.layer, interactableLayer))
-                hit.transform.GetComponent<AInteractableObject>()?.Interact();
+                interactable?.Interact();
         }
     }
 }
